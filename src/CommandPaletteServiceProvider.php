1fe56af4a02833a1d143346ed58a1eb1ee2a97b0
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:51:44 -0500

    Fix PHP class naming (PascalCase)

--- src/CommandPaletteServiceProvider.php
+++ src/CommandPaletteServiceProvider.php
@@ -3,14 +3,35 @@
 namespace MrShaneBarron\CommandPalette;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\CommandPalette\Livewire\CommandPalette;
+use MrShaneBarron\CommandPalette\View\Components\command-palette as BladeCommandPalette;
+use Livewire\Livewire;
 
 class CommandPaletteServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-command-palette.php', 'sb-command-palette');
+    }
+
     public function boot(): void
     {
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-command-palette', Livewire\CommandPalette::class);
-        }
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-command-palette');
+
+        Livewire::component('sb-command-palette', command-palette::class);
+
+        $this->loadViewComponentsAs('ld', [
+            BladeCommandPalette::class,
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-command-palette.php' => config_path('sb-command-palette.php'),
+            ], 'sb-command-palette-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-command-palette'),
+            ], 'sb-command-palette-views');
+        }
     }
 }
--- src/Livewire/CommandPalette.php
+++ src/Livewire/CommandPalette.php
@@ -6,59 +6,6 @@ use Livewire\Component;
 
 class CommandPalette extends Component
 {
-    public bool $open = false;
-    public string $search = '';
-    public array $commands = [];
-    public int $selectedIndex = 0;
-
-    public function mount(array $commands = []): void
-    {
-        $this->commands = $commands;
-    }
-
-    public function toggle(): void
-    {
-        $this->open = !$this->open;
-        $this->search = '';
-        $this->selectedIndex = 0;
-    }
-
-    public function updatedSearch(): void
-    {
-        $this->selectedIndex = 0;
-    }
-
-    public function selectNext(): void
-    {
-        $filtered = $this->getFilteredCommands();
-        $this->selectedIndex = min($this->selectedIndex + 1, count($filtered) - 1);
-    }
-
-    public function selectPrevious(): void
-    {
-        $this->selectedIndex = max($this->selectedIndex - 1, 0);
-    }
-
-    public function executeSelected(): void
-    {
-        $filtered = $this->getFilteredCommands();
-        if (isset($filtered[$this->selectedIndex])) {
-            $this->dispatch('command-executed', command: $filtered[$this->selectedIndex]);
-            $this->open = false;
-        }
-    }
-
-    public function getFilteredCommands(): array
-    {
-        if (empty($this->search)) {
-            return $this->commands;
-        }
-        return array_values(array_filter($this->commands, function ($cmd) {
-            return stripos($cmd['label'], $this->search) !== false ||
-                   (isset($cmd['keywords']) && stripos(implode(' ', $cmd['keywords']), $this->search) !== false);
-        }));
-    }
-
     public function render()
     {
         return view('sb-command-palette::livewire.command-palette');
